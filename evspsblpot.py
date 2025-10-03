9a335baa62b9a14bddd5fb32f2d21ded9dd72239
Damien Irving <irving.damien@gmail.com>
2025-10-03 15:16:43 +1000

    Add allen98 to evspsblpot

--- evspsblpot.py
+++ evspsblpot.py
@@ -12,7 +12,7 @@ import cmdline_provenance as cmdprov
 dask.diagnostics.ProgressBar().register()
 
 
-def fix_metadata(ds, input_ds):
+def fix_metadata(ds, input_ds, method):
     """Fix evspsblpot metadata"""
 
     try:
@@ -26,6 +26,7 @@ def fix_metadata(ds, input_ds):
     ds['time'].attrs = input_ds['time'].attrs
     ds['evspsblpot'].attrs['long_name'] = 'Potential Evapotranspiration'
     ds['evspsblpot'].attrs['standard_name'] = 'water_potential_evaporation_flux'
+    ds['evspsblpot'].attrs['method'] = method
 
     ds.attrs['variable_id'] = 'evspsblpot'
     ds.attrs['history'] = cmdprov.new_log()
@@ -36,17 +37,38 @@ def fix_metadata(ds, input_ds):
 def main(args):
     """Run the program."""
 
-    tasmax_ds = xr.open_mfdataset(args.tasmax_file)
-    tasmin_ds = xr.open_mfdataset(args.tasmin_file)
+    tasmax_ds = xr.open_dataset(args.tasmax_file)
+    tasmin_ds = xr.open_dataset(args.tasmin_file)
 
-    evspsblpot_da = xc.indices.potential_evapotranspiration(
-        tasmin=tasmin_ds['tasmin'],
-        tasmax=tasmax_ds['tasmax'],
-        method='HG85',
-    )
+    if args.method == 'hargreaves85':
+        evspsblpot_da = xc.indices.potential_evapotranspiration(
+            tasmin=tasmin_ds['tasmin'],
+            tasmax=tasmax_ds['tasmax'],
+            method=args.method,
+        )
+    elif args.method == 'allen98':
+        hurs_ds = xr.open_dataset(args.hurs_file)
+        sfcWind_ds = xr.open_dataset(args.sfcWind_file)
+        rsds_ds = xr.open_dataset(args.rsds_file)
+        rsus_ds = xr.open_dataset(args.rsus_file)
+        rlds_ds = xr.open_dataset(args.rlds_file)
+        rlus_ds = xr.open_dataset(args.rlus_file)
+        evspsblpot_da = xc.indices.potential_evapotranspiration(
+            tasmin=tasmin_ds['tasmin'],
+            tasmax=tasmax_ds['tasmax'],
+            hurs=hurs_ds['hurs'],
+            sfcWind=sfcWind_ds['sfcWind'],
+            rsds=rsds_ds['rsds'],
+            rsus=rsus_ds['rsus'],
+            rlds=rlds_ds['rlds'],
+            rlus=rlus_ds['rlus'],
+            method=args.method,
+        )
+    else:
+        raise ValueError(f'unrecognised method: {method}')
 
     evspsblpot_ds = evspsblpot_da.to_dataset(name='evspsblpot')
-    evspsblpot_ds = fix_metadata(evspsblpot_ds, tasmin_ds)
+    evspsblpot_ds = fix_metadata(evspsblpot_ds, tasmin_ds, args.method)
     evspsblpot_ds.to_netcdf(args.outfile)
 
 
@@ -55,8 +77,15 @@ if __name__ == '__main__':
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter
     )
-    parser.add_argument("tasmin_file", type=str, help="input daily minimum temperature file")
-    parser.add_argument("tasmax_file", type=str, help="input daily maximum temperature file")
     parser.add_argument("outfile", type=str, help="output file name")
+    parser.add_argument("method", type=str, choices=('hargreaves85', 'allen98'), help="method for calculating evspsblpot")
+    parser.add_argument("--tasmin_file", type=str, help="input daily minimum temperature file")
+    parser.add_argument("--tasmax_file", type=str, help="input daily maximum temperature file")
+    parser.add_argument("--hurs_file", type=str, help="input daily relative humidity file")
+    parser.add_argument("--sfcWind_file", type=str, help="input daily surface wind speed file")
+    parser.add_argument("--rsds_file", type=str, help="input daily surface downwelling shortwave file")
+    parser.add_argument("--rsus_file", type=str, help="input daily surface upwelling shortwave file")
+    parser.add_argument("--rlds_file", type=str, help="input daily surface downwelling longwave file")
+    parser.add_argument("--rlus_file", type=str, help="input daily surface upwelling longwave file")
     args = parser.parse_args()
     main(args)
--- evspsblpot.sh
+++ evspsblpot.sh
@@ -6,9 +6,15 @@
 python=/g/data/xv83/dbi599/miniconda3/envs/unseen/bin/python
 for tasmin_path in "$@"; do
     tasmax_path=`echo ${tasmin_path} | sed s:tasmin:tasmax:g`
-    evspsblpot_file=`basename ${tasmin_path} | sed s:tasmin:evspsblpot:g`
+    hurs_path=`echo ${tasmin_path} | sed s:tasmin:hurs:g`
+    sfcwind_path=`echo ${tasmin_path} | sed s:tasmin:sfcWind:g`
+    rsds_path=`echo ${tasmin_path} | sed s:tasmin:rsds:g`
+    rsus_path=`echo ${tasmin_path} | sed s:tasmin:rsus:g`
+    rlds_path=`echo ${tasmin_path} | sed s:tasmin:rlds:g`
+    rlus_path=`echo ${tasmin_path} | sed s:tasmin:rlus:g`
+    evspsblpot_file=`basename ${tasmin_path} | sed s:tasmin:evspsblpot-allen98:g`
     evspsblpot_path=/g/data/xv83/dbi599/treasury/${evspsblpot_file}
-    command="${python} evspsblpot.py ${tasmin_path} ${tasmax_path} ${evspsblpot_path}" 
+    command="${python} evspsblpot.py ${evspsblpot_path} allen98 --tasmin_file ${tasmin_path} --tasmax_file ${tasmax_path} --hurs_file ${hurs_path} --sfcWind_file ${sfcwind_path} --rsds_file ${rsds_path} --rsus_file ${rsus_path} --rlds_file ${rlds_path} --rlus_file ${rlus_path}" 
     echo ${command}
    ${command}   
 done
